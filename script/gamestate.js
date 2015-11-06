5d29a21f090d528ea587b12a0182a61b0ea45e17
Juz <juzley@gmail.com>
2015-11-06 23:43:08 +0000

    Start removing render module

--- script/gamestate.js
+++ script/gamestate.js
@@ -6,7 +6,6 @@ var money = 100;
 var cpu = 1;
 var ram = 1;
 var zombies = 0;
-var emails = [];
 var servers = [];
 var has_social_engineer = false;
 var has_developer = false;
@@ -15,16 +14,43 @@ function update() {
     money += 100;
 }
 
+function addEmail(subject, body, onRead) {
+    var item = $('<li>').addClass('email').appendTo('#emaillist');
+    var onClick = function() {
+        onRead();
+        $('.emailbody', item).toggle();
+        $('.emailsubject', item).removeClass('unread');
+    }
+
+    $('<div>')
+        .addClass('emailsubject')
+        .addClass('unread')
+        .text(subject)
+        .click(onClick)
+        .appendTo(item);
+    $('<div>')
+        .addClass('emailbody')
+        .text(body)
+        .attr('style', 'display: none')
+        .appendTo(item);
+}
+
 function init() {
+    $('#money').html('$' + money);
+    $('#cpu').html(cpu + ' core' + (cpu > 1 ? 's' : ''));
+    $('#ram').html(ram + ' gb');
+
     // Set up some dummy data.
     var addServer = function() {
         var server = new Terminal.Server("TestServer");
         servers.push(server);
         Terminal.Render.update();
     }
-    var email = new Terminal.Email("Subject", "Body", addServer);
-    emails.push(email);
-
+    addEmail("@@@Introduction@@@",
+             "@@@This email will introduce the player to the game," +
+             " and potentially add the first server?@@@",
+             addServer);
+    
     var addGroup = function() {
         var firewall = new Terminal.Server("Firewall", true, false, null);
         var server = new Terminal.Server("MainServer", false, false, firewall);
@@ -32,17 +58,12 @@ function init() {
         servers.push(group);
         Terminal.Render.update();
     }
-    email = new Terminal.Email("Subject 2", "Body 2", addGroup);
-    emails.push(email);
-
-    Terminal.Render.update();
+    addEmail("@@@Server Group@@@",
+             "@@@This email adds a server group@@@",
+             addGroup);
 }
 
 return {
-    "money": money,
-    "cpu": cpu,
-    "ram": ram,
-    "emails": emails,
     "servers": servers,
     "init": init
 };
--- script/render.js
+++ script/render.js
@@ -3,40 +3,6 @@ Terminal.Render = (function(Terminal, $) {
 "use strict";
 
 function update() {
-    $('#money').html('$' + Terminal.GameState.money);
-    $('#cpu').html(Terminal.GameState.cpu + ' core' +
-                   (Terminal.GameState.cpu > 1 ? 's' : ''));
-    $('#ram').html(Terminal.GameState.ram + ' gb');
-
-    $('#emaillist').empty();
-    var emails = Terminal.GameState.emails;
-    for (var i = 0; i < emails.length; i++) {
-        var email = emails[i];
-        var emailItem = $('<li>')
-            .addClass('email')
-            .appendTo('#emaillist');
-
-        var onClick = function(_email, _item) {
-            return function() {
-                _email.read();
-                $('.emailbody', _item).toggle();
-                $('.emailsubject', _item).removeClass('unread');
-            }
-        }(email, emailItem);
-
-        $('<div>')
-            .addClass('emailsubject')
-            .addClass('unread')
-            .text(email.subject)
-            .click(onClick)
-            .appendTo(emailItem);
-        $('<div>')
-            .addClass('emailbody')
-            .text(email.body)
-            //.attr('style', 'display: none')
-            .appendTo(emailItem);
-    }
-
     $('#serverlist').empty();
     var servers = Terminal.GameState.servers;
     for (var i = 0; i < servers.length; i++) {
